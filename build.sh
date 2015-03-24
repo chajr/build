1519313cd2c4049da5c11d016a9203b78768ddc0
chajr <chajr@bluetree.pl>
2015-03-24 19:56:09 +0100

    idylla build script
    Details of additions/deletions below:
    --------------------------------------------------------------
    
    Modified:   build.sh
                -- Added --
                    some build logic (read configurations and execute connection)
    
    Modified:   environments/example.sh.dist
                -- Updated --
                    added export command into example file

--- build.sh
+++ build.sh
@@ -1,1 +1,32 @@
 #!/usr/bin/env bash
+echo '';
+echo 'Build started';
+echo '';
+
+ENVIRONMENTS_LIST=`ls environments/`;
+BASE_DIR=`pwd`;
+
+for file in $ENVIRONMENTS_LIST
+do
+    if [ 'example.sh.dist' == $file ]
+    then
+        continue
+    fi
+
+    echo "Executing configuration from: $file";
+    . environments/$file;
+
+    if [ $HOST ]
+    then
+        echo "Connecting with remote host: $HOST";
+        ssh $HOST;
+    else
+        cd $DIR;
+    fi
+
+    cd $BASE_DIR;
+    echo '';
+done
+
+echo 'Build finished';
+echo '';
